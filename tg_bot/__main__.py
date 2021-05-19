d76a09fffed8a6bbaf11c59de99e95aa795428a7
pahanharsh <82528202+pahanharsh@users.noreply.github.com>
2021-05-19 08:36:35 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -42,7 +42,7 @@ Hello! my name *{}*.
  - /help <module name>: PM's you info about that module.
  - /settings:
    - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.https://telegra.ph//file/18af72fe1abdaa4a164ab.jpg
+   - in a group: will redirect you to pm, with all that chat's settings.(https://telegra.ph//file/18af72fe1abdaa4a164ab.jpg)
 
 
 {}
